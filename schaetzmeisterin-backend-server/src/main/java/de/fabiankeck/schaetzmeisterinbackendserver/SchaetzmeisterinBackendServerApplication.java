7c7c5b34306c9802e7aaab68385fb32c1e598382
Fabian Keck <fabian.keck@posteo.de>
2020-11-17 12:20:15 +0100

    intit Backend

---
+++ schaetzmeisterin-backend-server/src/main/java/de/fabiankeck/schaetzmeisterinbackendserver/SchaetzmeisterinBackendServerApplication.java
@@ -0,0 +1,13 @@
+package de.fabiankeck.schaetzmeisterinbackendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchaetzmeisterinBackendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchaetzmeisterinBackendServerApplication.class, args);
+	}
+
+}
---
+++ schaetzmeisterin-backend-server/src/test/java/de/fabiankeck/schaetzmeisterinbackendserver/SchaetzmeisterinBackendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fabiankeck.schaetzmeisterinbackendserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchaetzmeisterinBackendServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
