bbec1114bf365d496f455bbd6a88ad040f8aed58
Fabian Keck <fabian.keck@posteo.de>
2020-11-19 14:05:26 +0100

    Add PageLayout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,24 @@ import { Switch, Route } from 'react-router-dom';
 import SignInPage from './components/Pages/SignInPage';
 import GameContextProvider from './context/GameContextProvider';
 import GamePage from './components/Pages/GamePage';
+import styled from 'styled-components/macro';
 
 export default function App() {
   return (
     <GameContextProvider>
-      <Switch>
-        <Route path={'/game/:gameid'} component={GamePage} />
-        <Route path={'/signin/:gameid?'} component={SignInPage} />
-      </Switch>
+      <PageLayout>
+        <Switch>
+          <Route path={'/game/:gameid'} component={GamePage} />
+          <Route path={'/signin/:gameid?'} component={SignInPage} />
+        </Switch>
+      </PageLayout>
     </GameContextProvider>
   );
 }
+
+const PageLayout = styled.div`
+  display: grid;
+  grid-template-rows: min-content 1fr;
+  height: 100vh;
+  background-color: var(--brown-25);
+`;
