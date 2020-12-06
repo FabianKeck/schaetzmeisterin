b62febdccdb5fe800c4041aa9fabab3a1a29401d
Fabian Keck <fabian.keck@posteo.de>
2020-12-06 19:12:31 +0100

    Style Input

--- frontend/src/components/commons/Input.js
+++ frontend/src/components/commons/Input.js
@@ -6,9 +6,10 @@ export default function Input(props) {
 }
 
 const InputStyled = styled.input`
-  padding-left: var(--size-xxs);
-  border: none;
-  border-radius: var(--size-xs);
+  padding-left: var(--size-xs);
+  border: 1px solid var(--color-golden);
+  border-radius: var(--size-xxs);
   box-shadow: inset 0 0 var(--size-xs) #000;
+  background-color: var(--color-golden);
   width: 70%;
 `;
