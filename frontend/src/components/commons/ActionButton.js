5689608233fb5a036a61801d38360058a5ea0846
Fabian Keck <fabian.keck@posteo.de>
2020-12-06 19:12:47 +0100

    Add second rooster to header

--- frontend/src/components/commons/ActionButton.js
+++ frontend/src/components/commons/ActionButton.js
@@ -7,9 +7,10 @@ export default function ActionButton({ children, ...rest }) {
 
 const ActionButtonStyled = styled.button`
   border: 1px solid var(--color-golden);
-  box-shadow: none;
   border-radius: var(--size-xs);
   background-color: var(--color-active);
+  box-shadow: 2px 2px 2px #222;
+  font-family: 'EB Garamond', serif;
   padding: var(--size-xs);
   :disabled {
     background: none;
--- frontend/src/components/commons/Header.js
+++ frontend/src/components/commons/Header.js
@@ -7,24 +7,27 @@ export default function Header({ children }) {
     <HeaderStyled>
       <img src={Rooster} alt={'golden rooster'} />
       <HeadingStyled>{children}</HeadingStyled>
+      <img src={Rooster} alt={'golden rooster'} />
     </HeaderStyled>
   );
 }
 
 const HeaderStyled = styled.header`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   background: var(--color-main);
   padding: var(--size-s);
   img {
-    position: absolute;
-    left: var(--size-s);
     height: 40px;
+  }
+  img:nth-of-type(1) {
+    transform: scaleX(-1);
   }
 `;
 
 const HeadingStyled = styled.h1`
   margin: 0;
   color: var(--color-golden);
-  letter-spacing: 3px;
+  letter-spacing: 2px;
+  font-weight: normal;
 `;
